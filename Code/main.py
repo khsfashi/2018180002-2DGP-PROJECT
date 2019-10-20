703dfbac7858a3ca8f770da9bfd2ddd208036809
KHS <khsfashi@naver.com>
2019-10-21 00:57:20 +0900

    점프 완성
    점프 잘됨 ㅋㅋ루ㅋㅋ

--- Code/main.py
+++ Code/main.py
@@ -2,14 +2,19 @@ from pico2d import *
 import random
 
 # state : 캐릭터의 움직임을 저장하는 전역 변수
+# 0 : 아무것도 안 함, 1 : 오른쪽 움직임, -1 : 왼쪽 움직임, 2 : 문 열기
 state = 0
+Map_Size = [800, 450]
+jump = False
+
 
 class Map:
     def __init__(self):
         self.image = load_image('Resource\\BackGround\\Information_Room.png')
 
     def draw(self):
-        self.image.clip_draw(0, 0, 800, 450, 400, 300, 800, 600)
+        self.image.clip_draw(0, 0, Map_Size[0], Map_Size[1], 400, 300, 800, 600)
+
 
 class Player:
     def __init__(self):
@@ -17,7 +22,11 @@ class Player:
         self.x, self.y = 200, 200
         self.frame = 0
         self.dir = 0
+        self.acceleration = 6
+
     def update(self):
+        global state
+        global jump
         # 방향 정하기
         if state == 0:
             self.frame = 0
@@ -25,18 +34,29 @@ class Player:
             self.dir = 0
         elif state == -1:
             self.dir = 1
+        if jump == True:
+            self.frame = 1
 
         # 애니메이션
-        if state != 0:
+        if state == 1 or state == -1:
             self.frame = (self.frame + 1) % 3
 
         # 움직임
-        if state != 0:
+        if state == 1 or state == -1:
             if self.dir == 0:
                 self.x += 4
             elif self.dir == 1:
                 self.x -= 4
 
+        # 점프
+        if jump == True:
+            self.y += self.acceleration
+            self.acceleration -= 1
+            if self.acceleration == -10:
+                self.acceleration = 10
+                jump = False
+                if state != 1 and state != -1:
+                    state = 0
 
     def draw(self):
         self.image.clip_draw(self.frame * 32, self.dir * 32, 32, 32, self.x, self.y)
@@ -45,7 +65,10 @@ class Player:
 def handle_events():
     global running
     global state
+    global jump
+
     events = get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
             running = False
@@ -57,13 +80,14 @@ def handle_events():
                 state = -1
             elif event.key == SDLK_RIGHT:
                 state = 1
+            elif event.key == SDLK_SPACE:
+                jump = True
         # 키 뗄시 캐릭터 멈춤
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_LEFT and state == -1:
                 state = 0
             elif event.key == SDLK_RIGHT and state == 1:
                 state = 0
-
 
 
 open_canvas()
