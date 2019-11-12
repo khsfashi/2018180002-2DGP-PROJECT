462b0c37c6d5db01e1272aa92476d8de2adc8da4
KHS <khsfashi@naver.com>
2019-11-12 13:54:51 +0900

    리팩토링 5차 파일분할 2차
    Tile클래스 분할

--- Code/stage1_state.py
+++ Code/stage1_state.py
@@ -1,15 +1,16 @@
 import game_framework
 from pico2d import *
 from background import Background
+import tile
 import random
 
 # 상수 모음
 MAX_TILE_WIDTH = 25
 MAX_TILE_HEIGHT = 19
-TILE_WIDTH = 32
-TILE_HEIGHT = 32
 CHARACTER_WIDTH = 32
 CHARACTER_HEIGHT = 32
+TILE_WIDTH = 32
+TILE_HEIGHT = 32
 name = "Stage1State"
 
 # state : 캐릭터의 움직임을 저장하는 전역 변수
@@ -39,63 +40,6 @@ tile_Setting = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
                 [0, '■', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 ['■', '■', '■', '■', '■', '■', '■', '■', '■', '■', '■', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
                 ]
-
-def intersected_rectangle(collided_Rect, rect1_left, rect1_top, rect1_right, rect1_bottom,
-                          rect2_left, rect2_top, rect2_right, rect2_bottom):
-    vertical = False
-    horizontal = False
-    global jumping
-
-    if rect1_left <= rect2_right and rect1_right >= rect2_left:
-        horizontal = True
-        collided_Rect[0] = max(rect1_left, rect2_left)
-        collided_Rect[2] = min(rect1_right, rect2_right)
-
-    if rect1_top >= rect2_bottom and rect1_bottom <= rect2_top:
-        vertical = True
-        collided_Rect[1] = min(rect1_top, rect2_top)
-        collided_Rect[3] = max(rect1_bottom, rect2_bottom)
-
-    if vertical and horizontal:
-        return True
-    else:
-        return False
-
-class Tile:
-    image = None
-
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.collided_Rect = [0, 0, 0, 0]
-        self.isDraw = False
-        self.collided_Rect_Height = 0
-        self.collided_Rect_Width = 0
-        if Tile.image == None:
-            Tile.image = load_image('Resource\\TileSet\\Lab_Tile.png')
-
-    def draw(self):
-        if self.isDraw:
-            self.image.clip_draw(0, 0, TILE_WIDTH, TILE_HEIGHT, self.x, self.y, 32, 32)
-
-    def update(self, terra):
-        global jumping
-        if intersected_rectangle(self.collided_Rect, self.x - 16, self.y + 16, self.x + 16, self.y - 16,
-                                 terra.x - 10, terra.y + 16, terra.x + 10, terra.y - 16):
-            self.collided_Rect_Height = self.collided_Rect[1] - self.collided_Rect[3]
-            self.collided_Rect_Width = self.collided_Rect[2] - self.collided_Rect[0]
-
-            if self.collided_Rect_Width > self.collided_Rect_Height:
-                if self.collided_Rect[1] == self.y + 16:
-                    terra.y += self.collided_Rect_Height
-                    jumping = False
-                elif self.collided_Rect[3] == self.y - 16:
-                    terra.y -= self.collided_Rect_Height
-            else:
-                if self.collided_Rect[0] == self.x - 16:
-                    terra.x -= self.collided_Rect_Width
-                elif self.collided_Rect[2] == self.x + 16:
-                    terra.x += self.collided_Rect_Width
 
 
 class Terra:
@@ -143,7 +87,7 @@ def enter():
     global map, terra, background
     background = Background()
     terra = Terra()
-    map = [[Tile() for j in range(MAX_TILE_WIDTH)] for i in range(MAX_TILE_HEIGHT)]
+    map = [[tile.Tile() for j in range(MAX_TILE_WIDTH)] for i in range(MAX_TILE_HEIGHT)]
     for i in range(MAX_TILE_HEIGHT):
         for j in range(MAX_TILE_WIDTH):
             map[i][j].x = j * TILE_WIDTH + (TILE_WIDTH / 2)
@@ -204,7 +148,7 @@ def update():
         for j in range(MAX_TILE_WIDTH):
             if map[i][j].isDraw == True:
                 map[i][j].update(terra)
-                if intersected_rectangle(map[i][j].collided_Rect, map[i][j].x - 16, map[i][j].y + 16, map[i][j].x + 16,
+                if tile.intersected_rectangle(map[i][j].collided_Rect, map[i][j].x - 16, map[i][j].y + 16, map[i][j].x + 16,
                                  map[i][j].y - 16, terra.x - 10, terra.y + 16, terra.x + 10, terra.y - 16):
                     cnt = 1
     if cnt == 0:
---
+++ Code/tile.py
@@ -0,0 +1,59 @@
+from pico2d import *
+
+def intersected_rectangle(collided_Rect, rect1_left, rect1_top, rect1_right, rect1_bottom,
+                          rect2_left, rect2_top, rect2_right, rect2_bottom):
+    vertical = False
+    horizontal = False
+    global jumping
+
+    if rect1_left <= rect2_right and rect1_right >= rect2_left:
+        horizontal = True
+        collided_Rect[0] = max(rect1_left, rect2_left)
+        collided_Rect[2] = min(rect1_right, rect2_right)
+
+    if rect1_top >= rect2_bottom and rect1_bottom <= rect2_top:
+        vertical = True
+        collided_Rect[1] = min(rect1_top, rect2_top)
+        collided_Rect[3] = max(rect1_bottom, rect2_bottom)
+
+    if vertical and horizontal:
+        return True
+    else:
+        return False
+
+class Tile:
+    # 상수
+    image = None
+    TILE_WIDTH = 32
+    TILE_HEIGHT = 32
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.collided_Rect = [0, 0, 0, 0]
+        self.isDraw = False
+        self.collided_Rect_Height = 0
+        self.collided_Rect_Width = 0
+        if Tile.image == None:
+            Tile.image = load_image('Resource\\TileSet\\Lab_Tile.png')
+
+    def draw(self):
+        if self.isDraw:
+            self.image.clip_draw(0, 0, Tile.TILE_WIDTH, Tile.TILE_HEIGHT, self.x, self.y, 32, 32)
+
+    def update(self, terra):
+        if intersected_rectangle(self.collided_Rect, self.x - 16, self.y + 16, self.x + 16, self.y - 16,
+                                 terra.x - 10, terra.y + 16, terra.x + 10, terra.y - 16):
+            self.collided_Rect_Height = self.collided_Rect[1] - self.collided_Rect[3]
+            self.collided_Rect_Width = self.collided_Rect[2] - self.collided_Rect[0]
+
+            if self.collided_Rect_Width > self.collided_Rect_Height:
+                if self.collided_Rect[1] == self.y + 16:
+                    terra.y += self.collided_Rect_Height
+                elif self.collided_Rect[3] == self.y - 16:
+                    terra.y -= self.collided_Rect_Height
+            else:
+                if self.collided_Rect[0] == self.x - 16:
+                    terra.x -= self.collided_Rect_Width
+                elif self.collided_Rect[2] == self.x + 16:
+                    terra.x += self.collided_Rect_Width
